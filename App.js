150f46321d214f05fa87f6ecef6964cac98403b3
Ackerize <00078217@uca.edu.sv>
2022-05-24 10:40:07 -0600

    Configuracion de React Native Paper

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
+import {Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import reduxStore from './src/redux/store/store';
 import {PersistGate} from 'redux-persist/integration/react';
 import AppNavigation from './src/navigation/AppNavigation';
+
+const theme = {
+  ...DefaultTheme,
+  dark: false,
+};
 
 const App = () => {
   const {store, persistor} = reduxStore();
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
-          <AppNavigation />
-        </NavigationContainer>
+        <PaperProvider theme={theme}>
+          <NavigationContainer>
+            <AppNavigation />
+          </NavigationContainer>
+        </PaperProvider>
       </PersistGate>
     </Provider>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
