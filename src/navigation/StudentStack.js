c070edcd032faba208370bc846c8ca09e9d173fb
Cristian Mundo <37192145+AzzarCM@users.noreply.github.com>
2022-05-25 17:37:26 -0600

    feat: implemented results card without logic (#3)
    * added results modal
    
    * remove header title and added navigation to results

--- src/navigation/StudentStack.js
+++ src/navigation/StudentStack.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Home from '../screens/Home';
 import EditProfile from '../screens/EditProfile';
+import CaligrafyResults from '../screens/CaligrafyResults';
 
 const Stack = createStackNavigator();
 
@@ -28,6 +29,22 @@ export default function StudentStack() {
         component={EditProfile}
         options={{
           title: 'Editar perfil',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontSize: 22,
+            fontFamily: 'Sora-Medium',
+          },
+          headerBackImage: () => buttonLeft(),
+          headerStyle: {
+            backgroundColor: '#FBF5F2'
+          },
+        }}
+      />
+      <Stack.Screen
+        name='StudentResults'
+        component={CaligrafyResults}
+        options={{
+          title: 'Resultados',
           headerTitleAlign: 'center',
           headerTitleStyle: {
             fontSize: 22,
---
+++ src/screens/CaligrafyResults.js
@@ -0,0 +1,30 @@
+import {StyleSheet, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+import ResultComponent from '../components/Result/ResultWithPercentageIndicator'
+
+export default function CaligrafyResults() {
+  return (
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.mainContainer}>
+        <ResultComponent/>
+        <ResultComponent/>
+        <ResultComponent/>
+        <ResultComponent/>
+        <ResultComponent/>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: '#FBF5F2',
+    paddingVertical: 20,
+    alignItems: 'center',
+  },
+  title: {
+      textAlign: 'center',
+      fontSize: 19,
+      fontWeight: 'bold',
+  }
+});
