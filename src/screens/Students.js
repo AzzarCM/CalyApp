dfe13b85eb15db3e8efc07dcab1bcc44d75eb8d3
Cristian Mundo <cris.mundo@outlook.com>
2022-05-25 22:53:35 -0600

    fetching students

--- src/screens/Students.js
+++ src/screens/Students.js
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {getAllStudents} from '../api/student';
+import {useSelector} from 'react-redux';
 
 const Students = () => {
+  const [students, setStudents] = useState([]);
+  const {token} = useSelector(state => state.auth);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getAllStudents(token);
+      setStudents(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <View>
       <Text>Students</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Students
+export default Students;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
